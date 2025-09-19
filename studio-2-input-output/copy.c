b96f12323806ae150b170582df9d0cd642dedddc
Sahana Gujja <sahanagujja@Sahanas-MacBook-Air.local>
2025-09-18 21:58:31 -0500

    Completed Studio 2

--- studio-2-input-output/copy.c
+++ studio-2-input-output/copy.c
@@ -1,1 +1,27 @@
-//Replace this file with your copy.c
+#include <unistd.h>    // For read(), write()
+#include <stdlib.h>    // For exit()
+
+#define bufferSize 200
+
+int main() {
+    char buffer[bufferSize];
+    ssize_t bytes_read;
+    
+    while (1) {
+        bytes_read = read(STDIN_FILENO, buffer, bufferSize);
+        
+        if (bytes_read == 0) {
+            break;
+        }
+        
+        // Check for -1 for error
+        if (bytes_read == -1) {
+            // exit program if theres an error
+            exit(1);
+        }
+        
+        write(STDOUT_FILENO, buffer, bytes_read);
+    }
+    
+    return 0;
+}
